0cacae5e02bf58b5a74a85d5fc4a901f53ad4fb4
“OmarMahmoud” <omarmahmoudquraa@gmail.com>
2021-08-09 17:35:57 +0200

    handling one more case

--- Date formats transform/change_date_format.py
+++ Date formats transform/change_date_format.py
@@ -1,17 +1,17 @@
 import datetime
+
 new_date_list = []
 
 
 def change_date_format(date_list, date_format="%Y/%m/%d", i=0):
-    print(date_list)
     for date in date_list:
-        print(date, "begin")
         condition = True
         while condition:
             try:
                 date_ob = datetime.datetime.strptime(date, date_format)
                 new_date = date_ob.strftime("%Y%m%d")
-                new_date_list.append(new_date)
+                if date_format != "%Y%m%d":
+                    new_date_list.append(new_date)
                 i = 0
                 date_format = "%Y/%m/%d"
                 condition = False
@@ -21,9 +21,9 @@ def change_date_format(date_list, date_format="%Y/%m/%d", i=0):
                     date_format = "%d/%m/%Y"
                 elif i == 2:
                     date_format = "%m-%d-%Y"
+                elif i == 3:
+                    date_format = "%Y%m%d"
                 else:
+                    new_date_list.append("unsupported format")
                     condition = False
     print(new_date_list)
-
-
-change_date_format(["2021/12/01", "01/12/2020", "03-20-1996", "19630303"])
